371dd6937f94f4f4d6bc3abb1732ed7a310a2a5d
ilyjes00 <ilyjes00@gmail.com>
2023-11-07 17:44:53 +0900

    2023/11/07 체크상품수정, 수정폼

--- src/main/java/com/docmall/service/AdProductServiceImpl.java
+++ src/main/java/com/docmall/service/AdProductServiceImpl.java
@@ -1,13 +1,16 @@
 package com.docmall.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.docmall.domain.CategoryVO;
 import com.docmall.domain.ProductVO;
 import com.docmall.dto.Criteria;
+import com.docmall.dto.ProductDTO;
 import com.docmall.mapper.AdProductMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -48,6 +51,66 @@ public class AdProductServiceImpl implements AdProductService {
 	public List<ProductVO> pro_list(Criteria cri) {
 		// TODO Auto-generated method stub
 		return adProductMapper.pro_list(cri);
+	}
+
+
+
+
+
+
+
+	@Override
+	public void pro_checked_modify1(List<Integer> pro_num_arr, List<Integer> pro_price_arr, List<String> pro_buy_arr) {
+		
+		//db의 연동작업이 size만큼 진행이된다
+		//Connection Open -> mapper.xml sql구문실행 -> Connection Close
+		for(int i=0; i<pro_num_arr.size(); i++) {
+			adProductMapper.pro_checked_modify1(pro_num_arr.get(i), pro_price_arr.get(i), pro_buy_arr.get(i));
+		}
+	}
+
+
+
+
+
+
+
+	@Override
+	public void pro_checked_modify2(List<Integer> pro_num_arr, List<Integer> pro_price_arr, List<String> pro_buy_arr) {
+		// TODO Auto-generated method stub
+		List<ProductDTO> pro_modify_list = new ArrayList<ProductDTO>();
+		
+		for(int i=0; i<pro_num_arr.size();i++) {
+			ProductDTO productDTO = new ProductDTO(pro_num_arr.get(i), pro_price_arr.get(i), pro_buy_arr.get(i));
+			pro_modify_list.add(productDTO);
+		}
+		
+		adProductMapper.pro_checked_modify2(pro_modify_list);
+			
+	}
+
+
+
+
+
+
+
+	@Override
+	public ProductVO pro_edit(Integer pro_num) {
+		// TODO Auto-generated method stub
+		return adProductMapper.pro_edit(pro_num);
+	}
+
+
+
+
+
+
+
+	@Override
+	public CategoryVO get(Integer cg_code) {
+		// TODO Auto-generated method stub
+		return adProductMapper.get(cg_code);
 	}
 	
 }
